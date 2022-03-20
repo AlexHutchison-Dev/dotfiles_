2855d618b37a565e587a5be24d17f004abb944a0
Alex Hutchison <a.hutchison@live.co.uk>
2022-03-20 14:06:46 -0700

    Revert "Configured project to use setuptools for pip installation"
    This reverts commit 2b7d79d3a523f77f42ae6e5b470cfd450484a558.

--- dotfiles.py
+++ dotfiles.py
@@ -1,9 +1,14 @@
+#!/bin/python3
 import click
 
 
 @click.command()
-def cli():
+def main():
     """
     A cli program for managing dotfiles
     """
-    click.echo("Hello World!")
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
